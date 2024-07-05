075307579f1b18a9bc5310573aa4f47d2e2f2f96
Jan <zhuojun.jian@litevar.com>
2024-07-05 09:24:03 +0800

    match service UserMessageDtoList input

--- bin/server.dart
+++ bin/server.dart
@@ -7,21 +7,22 @@ import '../lib/src/router.dart';
 import '../lib/src/middleware.dart';
 import '../lib/src/util/logger.dart';
 
-
 void main(List<String> args) async {
   await startServer();
 }
 
 Future<void> startServer() async {
-
   final Router router = Router();
   apiRoutes();
   router.mount(config.server.apiPathPrefix, apiRouter);
   Handler handler = const Pipeline()
-      .addMiddleware(checkAuthorization("111"))
+      //.addMiddleware(checkAuthorization("<ADD_YOUR_AUTHORIZATION_HERE>"))
       .addMiddleware(logRequest())
       .addHandler(router);
 
-  HttpServer server = await serve(handler, config.server.ip, config.server.port);
-  logger.log(LogModule.http, "Start Server", detail: "http://${server.address.host}:${server.port}${config.server.apiPathPrefix}");
+  HttpServer server =
+      await serve(handler, config.server.ip, config.server.port);
+  logger.log(LogModule.http, "Start Server",
+      detail:
+          "http://${server.address.host}:${server.port}${config.server.apiPathPrefix}");
 }
